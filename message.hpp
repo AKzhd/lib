a0af92ae082cae4926e426418ac1279f23043427
Virtual Andrew <andreo1111@gmail.com>
2023-06-22 00:52:06 +0500

    add message

--- message.hpp
+++ message.hpp
@@ -4,3 +4,19 @@
 #include <fstream>
 
 class Message
+{
+    private:
+    std::string _text;
+    std::string _sender;
+    std::string _receiver;
+    public:
+    Message(const std::string &text, const std::string &sender, const std::string &receiver) : name(_name), login(_login), pass(_pass) {}
+    
+    const std::string &getText() const { return _text; }
+
+    const std::string &getSender() const { return _sender; }
+   
+    const std::string &getReceiver() const { return _receiver; }
+
+    ~User();
+};
